900f8b49fa1fdfdf8702f3bbc2c379ceddf57b1e
vikikota <viktoria.kota@gmail.com>
2024-08-20 11:32:15 +0200

    024 task done

---
+++ 024/app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: "",
+      confirmedName: ""
+    };
+  },
+  methods: {
+    confirmInput(){
+      this.confirmedName = this.name
+    },
+    submitForm(){
+      alert("YEAH!")
+    },
+    setName(event, lastName){
+      this.name = event.target.value + " " + lastName
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+    },
+  },
+});
+
+app.mount("#events");
